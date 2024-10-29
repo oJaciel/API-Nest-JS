e064c2b2b9074875873ddc85e063614b54138943
Jaciel <jacielhbarichello@gmail.com>
2024-10-28 21:43:24 -0300

    Validações ao Projeto
    Adicionadas validações nos atributos via Class Validator

--- src/dtos/create-student-dto.ts
+++ src/dtos/create-student-dto.ts
@@ -1,5 +1,18 @@
+import { IsNotEmpty, Length } from "class-validator";
+
 export class CreateStudentDTO {
+
+    @IsNotEmpty({
+        message: 'O nome é obrigatório'
+    })
+    @Length(3, 100)
     name?: string;
+
+    @IsNotEmpty({
+        message: 'O e-mail é obrigatório'
+    })
     email?: string;
+
+    
     age?: number;
 }
